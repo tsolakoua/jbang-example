2eb8b98e2fe7b0ebc685ac60369e1d6e8d2be1c8
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-07-21 15:08:57 +0200

    add calculator

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+class Calculator {
+    public static void main(String args[]) {
+        System.out.println(division(1, 0));
+    }
+
+    public static double division(int a, int b) {
+        return a/b;
+    }
+}
